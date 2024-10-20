824ca6379f5ee7b623a57793dd34e4f9b5c20869
StrongVector <vector.strong@protonmail.com>
2024-10-20 13:45:32 +0900

    author

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
    <footer>
-    <p className='text-center text-lg p-4'>Developed By <a className='underline text-blue-500' href="https://ankitjha.vercel.app" target='_blank'>Ankit Jha</a></p>
+    <p className='text-center text-lg p-4'>Developed By <a className='underline text-blue-500' href="https://ankitjha.vercel.app" target='_blank'>Enrique Negri</a></p>
    </footer>
   )
 }
